6ee1cf0743240db5fa2fa71227f466fb22e6df9f
Viktoria0407 <viktoryiafomchenko@gmail.com>
2021-08-10 16:40:49 +0300

    start HRN

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
+    const toReadable = (number) => {
+        const lastChar = number[number.length - 1];
+        if (lastChar === '1') {
+          return 'one';
+        }
+          if (lastChar === '2') {
+          return 'two';
+        }
+           if (lastChar === '3') {
+          return 'three';
+        }
+           if (lastChar === '4') {
+          return 'four';
+        }
+          if (lastChar === '5') {
+          return 'five';
+        }
+          if (lastChar === '6') {
+          return 'six';
+        }
+          if (lastChar === '7') {
+          return 'seven';
+        }
+          if (lastChar === '8') {
+          return 'eight';
+        }
+          if (lastChar === '9') {
+          return 'nine';
+        }
+      
+        return '';
+      };
 }
