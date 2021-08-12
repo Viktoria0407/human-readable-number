6058345c53e56198a68a0b5539a3159cce12e18f
Viktoria0407 <viktoryiafomchenko@gmail.com>
2021-08-12 21:40:50 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-module.exports = function toReadable (number) {
-
-    let unitsNumber = [
+module.exports = function toReadable(number) {
+    let arrBefore9 = [
         "zero",
         "one",
         "two",
@@ -12,7 +11,7 @@ module.exports = function toReadable (number) {
         "eight",
         "nine",
     ];
-    let dozensUnitsNumber = [
+    let arrBefore19 = [
         "ten",
         "eleven",
         "twelve",
@@ -24,7 +23,7 @@ module.exports = function toReadable (number) {
         "eighteen",
         "nineteen",
     ];
-    let dozensNumbers = [
+    let arrBefore99 = [
         "twenty",
         "thirty",
         "forty",
@@ -36,12 +35,55 @@ module.exports = function toReadable (number) {
     ];
 
     if (number >= 0 && number < 10) {
-        return units[number];
+        return arrBefore9[number];
     }
 
     if (number >= 10 && number < 20) {
-        return dozensUnitsNumber[number % 10];
+        return arrBefore19[number % 10];
     }
 
-    
+    if (number >= 20 && number < 100) {
+        let dozensNumber = arrBefore99[Math.floor(number / 10) - 2];
+        let unitsNumber = arrBefore9[number % 10];
+        if (number % 10 === 0) {
+            return dozensNumber;
+        } else {
+            return dozensNumber + " " + unitsNumber;
+        }
+    }
+
+    if (number >= 100 && number < 1000) {
+        let hundredNumber = arrBefore9[Math.floor(number / 100)];
+        let dozensNumber = arrBefore99[Math.floor(number / 10) % 10];
+        let unitsNumber = arrBefore9[number % 10];
+        if (Math.floor(number / 10) % 10 === 0 && number % 10 === 0) {
+            return hundredNumber + " hundred";
+        }
+        else if (number % 100 >= 10 && number % 100 < 20) {
+            return hundredNumber + " hundred " + arrBefore19[number % 10];
+        }
+        else if (
+            number % 100 >= 20 &&
+            number % 100 < 100 &&
+            number % 10 === 0
+        ) {
+            return (
+                hundredNumber +
+                " hundred " +
+                arrBefore99[(Math.floor(number / 10) % 10) - 2]
+            );
+        } else if (Math.floor(number / 10) % 10 === 0) {
+            return hundredNumber + " hundred " + unitsNumber;
+        }
+        else {
+            return (
+                hundredNumber +
+                " hundred " +
+                arrBefore99[(Math.floor(number / 10) % 10) - 2] +
+                " " +
+                arrBefore9[number % 10]
+            );
+        }
+    }
 };
+
