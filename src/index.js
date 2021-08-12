e6fd80daab2f0b5a6c74cddaf4d69b9535ea0626
Viktoria0407 <viktoryiafomchenko@gmail.com>
2021-08-12 21:20:45 +0300

    add arr

--- src/index.js
+++ src/index.js
@@ -1,34 +1,47 @@
 module.exports = function toReadable (number) {
-    const toReadable = (number) => {
-        const lastChar = number[number.length - 1];
-        if (lastChar === '1') {
-          return 'one';
-        }
-          if (lastChar === '2') {
-          return 'two';
-        }
-           if (lastChar === '3') {
-          return 'three';
-        }
-           if (lastChar === '4') {
-          return 'four';
-        }
-          if (lastChar === '5') {
-          return 'five';
-        }
-          if (lastChar === '6') {
-          return 'six';
-        }
-          if (lastChar === '7') {
-          return 'seven';
-        }
-          if (lastChar === '8') {
-          return 'eight';
-        }
-          if (lastChar === '9') {
-          return 'nine';
-        }
-      
-        return '';
-      };
-}
+
+    let unitsNumber = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let dozensUnitsNumber = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let dozensNumbers = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number >= 0 && number < 10) {
+        return units[number];
+    }
+
+    if (number >= 10 && number < 20) {
+        return dozensUnitsNumber[number % 10];
+    }
+
+    
+};
